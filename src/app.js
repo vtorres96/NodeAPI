be3e1c65fa76352cbd1bbe943acdb26cc706d2f4
vtorres96 <victorcdc96@gmail.com>
2019-01-11 18:57:05 -0200

    habilitando cors

--- src/app.js
+++ src/app.js
@@ -20,10 +20,20 @@ const productRoute = require('./routes/productRoute')
 const customerRoute = require('./routes/customerRoute')
 const orderRoute = require('./routes/orderRoute')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({
+    limit: '5mb'
+}))
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+// Hability the CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next()
+})
 
 app.use('/', indexRoute)
 app.use('/products', productRoute)
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo a Node Store!';
 
 module.exports = {
-    connectionString: 'SUA CONNECTION STRING',
-    sendgridKey: 'SUA API KEY',
+    connectionString: 'mongodb://vtorres96:03v01t96m@ds046549.mlab.com:46549/nodestory',
+    sendgridKey: 'SSG.AHXLC-bPS9q2G1IaZ4Kgrg.r4fZaY3DvLtpUVg8ujOYmx6pNlb3uk-UeC85X2BxF6Q',
     containerConnectionString: 'SUA CONNECTION STRING'
 }
